191201eb4c08d942e19c8c613f919818a7547f29
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-10-26 18:08:48 +0200

    Level 21 done

---
+++ PlayersGuide/Level21Static/Arrow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Level21Static.Arrowhead;
+using static Level21Static.Fletching;
+
+namespace Level21Static
+{
+    public class Arrow
+    {
+        public ArrowheadType Arrowhead { get; }
+        public FletchingType Fletching { get; }
+        public float Length { get; }
+        public Arrow(ArrowheadType arrowhead, FletchingType fletching, float length)
+        {
+            Arrowhead = arrowhead;
+            Fletching = fletching;
+            Length = length;
+        }
+
+        public static Arrow CreateEliteArrow()
+        {
+            return new Arrow(ArrowheadType.Steel, FletchingType.Plastic, 95);
+        }
+
+        public static Arrow CreateBeginnerArrow()
+        {
+            return new Arrow(ArrowheadType.Wood, FletchingType.GooseFeathers, 75);
+        }
+
+        public static Arrow CreateMarksmanArrow()
+        {
+            return new Arrow(ArrowheadType.Steel, FletchingType.GooseFeathers, 65);
+        }
+        public float GetCost()
+        {
+            float arrowheadCost = Arrowhead switch
+            {
+                ArrowheadType.Steel => 10,
+                ArrowheadType.Wood => 3,
+                ArrowheadType.Obsidian => 5,
+
+
+            };
+            float fletchingCost = Fletching switch
+            {
+                FletchingType.Plastic => 10,
+                FletchingType.TurkeyFeathers => 5,
+                FletchingType.GooseFeathers => 3
+            };
+            float shaftCost = 0.05f * Length;
+            return arrowheadCost + fletchingCost + shaftCost;
+        }
+    }
+}
---
+++ PlayersGuide/Level21Static/Program.cs
@@ -0,0 +1,70 @@
+﻿using Extensions;
+using static Level21Static.Arrowhead;
+using static Level21Static.Fletching;
+
+namespace Level21Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What type of arrow do you want?");
+            Console.WriteLine("1. Elite arrow");
+            Console.WriteLine("2. Beginner arrow");
+            Console.WriteLine("3. Marksman arrow");
+            Console.WriteLine("4. Custom arrow");
+            string? choice = Console.ReadLine();
+            Arrow arrow = choice switch
+            {
+                "1" => Arrow.CreateEliteArrow(),
+                "2" => Arrow.CreateBeginnerArrow(),
+                "3" => Arrow.CreateMarksmanArrow(),
+                "4" => GetCustomArrow(),
+                _ => throw new KeyNotFoundException()
+            };
+            Console.WriteLine($"This arrow costs {arrow.GetCost()} gold.");
+
+            Arrow GetCustomArrow()
+            {
+                ArrowheadType arrowhead = GetArrowheadType();
+                FletchingType fletching = GetFletchingType();
+                float length = GetShaftLength();
+
+                return new Arrow(arrowhead, fletching, length);
+            }
+
+            ArrowheadType GetArrowheadType()
+            {
+                Console.WriteLine("What type of arrowhead do you want? (steel, wood, obsidian) ");
+                string? input = Console.ReadLine();
+                return input switch
+                {
+                    "steel" => ArrowheadType.Steel,
+                    "wood" => ArrowheadType.Wood,
+                    "obsidian" => ArrowheadType.Obsidian,
+                    _ => throw new KeyNotFoundException(),
+                };
+            }
+
+            FletchingType GetFletchingType()
+            {
+                Console.WriteLine("What type of fletching do you want? (plastic, turkey feathers, goose feathers)");
+                string? input = Console.ReadLine();
+                return input switch
+                {
+                    "plastic" => FletchingType.Plastic,
+                    "turkey feathers" => FletchingType.TurkeyFeathers,
+                    "goose feathers" => FletchingType.GooseFeathers,
+                    _ => throw new KeyNotFoundException()
+                }; ;
+            }
+
+            float GetShaftLength()
+            {
+                float length = TakingANumber.AskForNumberInRange("How long do you want the arrow to be? (Between 60 and 100 cm)", 60, 100);
+                return length;
+            }
+        }
+    }
+}
