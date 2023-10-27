d8fa95c62ad993ae2665ba9f5a9f35478654b7ca
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-10-27 11:08:26 +0200

    Point Boss Battle done

---
+++ PlayersGuide/Level24Catacombs/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level24Catacombs
+{
+    public class Point
+    {
+        public float XCoordinate { get; }
+        public float YCoordinate { get; }
+
+
+        public Point(float x, float y)
+        {
+            XCoordinate = x;
+            YCoordinate = y;
+        }
+
+        public Point() : this(0, 0)
+        {
+
+        }
+
+        //The properties in my code are immutable.
+        //There is no reason to make them mutable, since our program does not require the user to provide coordinates, nor do other classes require setter access.
+    }
+}
---
+++ PlayersGuide/Level24Catacombs/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Level24Catacombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point();
+        }
+
+        static void Point()
+        {
+            Point One = new Point(2, 3);
+            Point Two = new Point(-4, 0);
+
+            Console.WriteLine($"({One.XCoordinate}, {One.YCoordinate})");
+            Console.WriteLine($"({Two.XCoordinate}, {Two.YCoordinate})");
+        }
+    }
+}
