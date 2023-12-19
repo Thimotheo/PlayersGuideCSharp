8673f403ece30accaebb168d347d1176c3107dc6
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-12-19 13:28:02 +0100

    Level 30 done

---
+++ PlayersGuide/Level30Generics/ColoredItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level30Generics
+{
+    public class ColoredItem<T>
+    {
+        public T Item { get; }
+        public ConsoleColor Color { get; }
+
+        public ColoredItem(T item, ConsoleColor color)
+        {
+            Item = item;
+            Color = color;
+        }
+
+        public void Display()
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine(Item?.ToString());
+        }
+    }
+}
---
+++ PlayersGuide/Level30Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Level30Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+            ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+            ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+            sword.Display();
+            bow.Display();
+            axe.Display();
+        }
+    }
+}
