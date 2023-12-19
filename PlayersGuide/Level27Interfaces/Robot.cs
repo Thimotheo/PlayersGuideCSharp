a7e873c88d61b2f1ea8ef20435f3797a756ed5e8
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-12-19 10:52:37 +0100

    Levelm 27 done and working

---
+++ PlayersGuide/Level27Interfaces/Robot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level27Interfaces
+{
+    public class Robot : IRobotCommand
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsPowered { get; set; }
+        public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+        public void Run(Robot robot)
+        {
+            foreach (IRobotCommand? command in Commands)
+            {
+                command?.Run(this);
+                Console.WriteLine($"[{X} {Y} {IsPowered}]");
+            }
+        }
+    }
+}
---
+++ PlayersGuide/Level27Interfaces/SouthCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level27Interfaces
+{
+    internal class SouthCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered) { robot.Y--; }
+        }
+    }
+}
