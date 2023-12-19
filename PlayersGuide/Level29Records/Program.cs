c24657009290c744535f48c9e8a10c74c62986ac
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-12-19 12:03:01 +0100

    Level 29 records done

---
+++ PlayersGuide/Level29Records/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Level29Records
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sword basicSword = new Sword(MaterialType.Iron, GemstoneType.None, 8, 0.25f);
+            Sword fancySword = basicSword with { Material = MaterialType.Binarium, Gemstone = GemstoneType.Bitstone };
+            Sword redSword = basicSword with { Material = MaterialType.Bronze, Gemstone = GemstoneType.Amber };
+
+            Console.WriteLine(basicSword);
+            Console.WriteLine(fancySword);
+            Console.WriteLine(redSword);
+        }
+        public record Sword(MaterialType Material, GemstoneType Gemstone, float Length, float CrossguardWidth);
+
+        public enum MaterialType { Wood, Bronze, Iron, Steel, Binarium }
+        public enum GemstoneType { Emerald, Amber, Sapphire, Diamond, Bitstone, None }
+    }
+}
