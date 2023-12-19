940cacff48f2dbb84b4faa7ff5f5b8c3f8153d07
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-12-19 11:38:38 +0100

    Level 28 structs done and working

---
+++ PlayersGuide/Level28Structs/Coordinate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level28Structs
+{
+    public struct Coordinate
+    {
+        public int Row { get; }
+        public int Column { get; }
+        public Coordinate(int row, int column) { Row = row; Column = column; }
+
+        public static bool CheckIfAdjacent(Coordinate a, Coordinate b)
+        {
+            int rowDifference = a.Row - b.Row;
+            int columnDifference = a.Column - b.Column;
+
+            if (Math.Abs(rowDifference) <= 1 && columnDifference == 0)
+                return true;
+
+            if (Math.Abs(columnDifference) <= 1 && rowDifference == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ PlayersGuide/Level28Structs/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Level28Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinate a = new Coordinate(1, 1);
+            Coordinate b = new Coordinate(1, 2);
+            Coordinate c = new Coordinate(2, 2);
+
+            Console.WriteLine(Coordinate.CheckIfAdjacent(a, b));
+            Console.WriteLine(Coordinate.CheckIfAdjacent(b, c));
+            Console.WriteLine(Coordinate.CheckIfAdjacent(a, c));
+
+        }
+    }
+}
